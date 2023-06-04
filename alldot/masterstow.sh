e6c9083e2986be12d0674ba41a558052e537acad
Parth <poproot4@gmail.com>
2023-06-05 04:27:48 +0530

    Stow update

---
+++ alldot/masterstow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo -e "\nUpdating all file\nconecting all file\n.....\n...........\n....................."
+cd i3
+stow --verbose=3 ./ -t ~/
+cd ..
+cd kitty
+stow --verbose=3 ./ -t ~/
+cd ..
+cd polybar
+stow --verbose=3 ./ -t ~/
+cd ..
+cd rofi
+stow --verbose=3 ./ -t ~/
+cd ..
+
+
+
