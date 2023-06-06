fff659d2f450187f665297174aadaabe96e5d4e1
Parth <poproot4@gmail.com>
2023-06-07 02:13:25 +0530

    auto update

--- prograssbar.sh
+++ prograssbar.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# Author : Teddy Skarin
-
-# 1. Create ProgressBar function
-# 1.1 Input is currentState($1) and totalState($2)
-function ProgressBar {
+# Author : Teddy Skarin(OC) for progress bar and Parth-root(eficint)
+function pbar {
 # Process data
-	let _progress=(${1}*100/${2}*100)/100
+	let _progress=(${1}*100/100*100)/100
 	let _done=(${_progress}*4)/10
 	let _left=40-$_done
 # Build progressbar string lengths
 	_done=$(printf "%${_done}s")
 	_left=$(printf "%${_left}s")
-
-# 1.2 Build progressbar strings and print the ProgressBar line
-# 1.2.1 Output example:
-# 1.2.1.1 Progress : [########################################] 100%
-tput cup $LINES 0;
-printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
-
+#tput civis
+tput sc #save the current cursor position
+tput cup $((`tput lines`-1)) 3
+printf "\b\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
+tput rc
+sleep 1
+#tput cnorm
 }
+
 
 # Variables
 _start=1
@@ -30,13 +28,18 @@ _end=100
 # Proof of concept
 sleep 1
 echo lol
-ProgressBar 1 100
+pbar 1 
 sleep 1
 echo start
-ProgressBar 20 100
-sleep 3
+echo "is new file"
+pbar 20
 echo lol
-ProgressBar 50 100
-sleep 4
-ProgressBar 100 100
+echo "lol"
+sleep 1
+pbar 50
+sleep 1
+echo lol
+pbar 30
+sleep 1
+pbar 100
 printf '\nFinished!\n'
